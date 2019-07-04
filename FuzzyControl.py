91079b22d3e21b7f992e9014657420282e8ce0c6
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-04 15:53:22 -0500

    Update FuzzyControl.py

--- FuzzyControl.py
+++ FuzzyControl.py
@@ -11,6 +11,7 @@ import time
 
 Vrefd = ctrl.Consequent(np.arange(-2, 2, 0.1), 'Vrefd')
 Pdif = ctrl.Antecedent(np.arange(-200, 200, 0.01),'Pdif')
+Vdif = ctrl.Antecedent(np.arange(-2, 2, 0.1),'Vdif')
 
 #Membership functions
 
@@ -20,24 +21,44 @@ Pdif['N'] = fuzz.trapmf(Pdif.universe, [-100,-2, -0.2, -0.01])
 Pdif['P'] = fuzz.trapmf(Pdif.universe, [0.01, 0.2, 2, 100])
 Pdif['Z'] = fuzz.trimf(Pdif.universe, [-0.01,0,0.01])
 
+#Vdif
+
+Vdif['N'] = fuzz.trimf(Vdif.universe, [-0.3, -0.2, -0.1])
+Vdif['P'] = fuzz.trimf(Vdif.universe, [0.1, 0.2, 0.3])
+Vdif['Z'] = fuzz.trimf(Vdif.universe, [-0.01, 0, 0.01])
+
+
 
 #Vref
 
-Vrefd['N'] = fuzz.trimf(Vrefd.universe, [-0.2, -0.1, 0])
-Vrefd['P'] = fuzz.trimf(Vrefd.universe, [0, 0.1, 0.2])
+Vrefd['N'] = fuzz.trimf(Vrefd.universe, [-0.3, -0.2, -0.1])
+Vrefd['P'] = fuzz.trimf(Vrefd.universe, [0.1, 0.2, 0.3])
 Vrefd['Z'] = fuzz.trimf(Vrefd.universe, [-0.01, 0, 0.01])
 
 ##Rules
 
-rule1=ctrl.Rule(Pdif['P'],Vrefd['P'])
-rule2=ctrl.Rule(Pdif['N'],Vrefd['N'])
-rule3=ctrl.Rule(Pdif['Z'],Vrefd['Z'])
+#rule1=ctrl.Rule(Pdif['P'],Vrefd['P'])
+#rule2=ctrl.Rule(Pdif['N'],Vrefd['N'])
+#rule3=ctrl.Rule(Pdif['Z'],Vrefd['Z'])
+rule1=ctrl.Rule(Pdif['P']&Vdif['N'],Vrefd['N'])
+rule2=ctrl.Rule(Pdif['N']&Vdif['P'],Vrefd['N'])
+rule3=ctrl.Rule(Pdif['N']&Vdif['N'],Vrefd['P'])
+rule4=ctrl.Rule(Pdif['P']&Vdif['P'],Vrefd['P'])
+rule5=ctrl.Rule(Pdif['Z'],Vrefd['Z'])
 
-vref_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+vref_ctrl = ctrl.ControlSystem([rule1, rule2, rule3,rule4, rule5])
 vrefout = ctrl.ControlSystemSimulation(vref_ctrl)
 
 i= True
-v=18.3
+Pdif=1
+Vrefin=-0.2
+V2=18.6
+
+n = excel.main(float(V2),0)
+n = int(n)
+mcpras.set_value(n)
+P2=Node611.sensorm()
+
 while True:
   
     if v<14.6 :
@@ -45,30 +66,35 @@ while True:
     elif v>18.5:
       v=18.6
       
-        
-    n = excel.main(float(v),0)
-    n = int(n)
-    mcpras.set_value(n)
-    P1=Node611.sensorm()
-   
-    time.sleep(0.5)
+    vrefout.input['Pdif']=Pdif
+    vrefout.input['Vdif']=Vrefin
+    vrefout.compute()
+    Vrefin=round(vrefout.output['Vrefd'],2)
     
-    n = excel.main(float(v+0.2),0)
+    v=v2
+    v2=v2+Vrefin
+    P1=P2
+    
+    n = excel.main(float(v2),0)
     n = int(n)
     mcpras.set_value(n)
     P2=Node611.sensorm()
-    
-    
+  
     Pdif=P2-P1
   
-    if (abs(Pdif)<0.5): 
+    time.sleep(1)
+    
+  #  n = excel.main(float(v+0.2),0)
+  #  n = int(n)
+  #  mcpras.set_value(n)
+  #  P2=Node611.sensorm()
+    
+    
+    
+    if (abs(Pdif)<0.6): 
        Pdif=0
          
-    vrefout.input['Pdif']=Pdif
-    vrefout.compute()
-    Vrefin=round(vrefout.output['Vrefd'],2)
-    v=v+Vrefin
-  
+    
     print("Potencia del panel t= "+str(P1))
     print("Potencia del panel  t+1 = "+str(P2))
     print("Vref = "+str(v))
