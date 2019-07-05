974d24fb5da24bd087b73f669f42bd8b87c45b4b
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-05 15:08:42 -0500

    Update Pruebas.py

--- Pruebas.py
+++ Pruebas.py
@@ -10,7 +10,7 @@ archivo=open("prueba1.txt","w")
 while i<41:
   
    
-    n = excel.main(float(16),0)
+    n = excel.main(float(v+0.1*i),0)
     n = int(n)
     mcpras.set_value(n)
     time.sleep(1)
@@ -18,12 +18,12 @@ while i<41:
     archivo.write(str(P1)+'\n')
    
     
-    n = excel.main(float(16.2),0)
-    n = int(n)
-    mcpras.set_value(n)
-    time.sleep(1)
-    P1=Node611.sensorm()
-    archivo.write(str(P1)+'\n')
+ #   n = excel.main(float(16.2),0)
+ #   n = int(n)
+ #   mcpras.set_value(n)
+ #   time.sleep(1)
+ #   P1=Node611.sensorm()
+ #   archivo.write(str(P1)+'\n')
     i=i+1
     
     
