4a6d275c8e0d0b510a4f0dde018cc9efdfed1265
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-10 11:52:54 -0500

    Update adquisicion.py

--- adquisicion.py
+++ adquisicion.py
@@ -13,7 +13,7 @@ import timeit as tm
 #Configuration SPI Port and device
 SPI_PORT   = 0
 SPI_DEVICE = 0
-S=0
+sw=0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 #Configuration pin output
@@ -22,7 +22,7 @@ GPIO.setmode(GPIO.BCM)
 
 
 def adquisicion (i):
-	global S
+	global sw
 	while i==1:     
 		tic = tm.default_timer()
 		A2 = mcp.read_adc(7)
@@ -48,7 +48,7 @@ def adquisicion (i):
    
 		print(S_2)
 		print(toc-tic)
-		S=1
+		sw=1
 		#print("Potencia del panel = "+Pp)
 		#print("Voltaje del panel = "+Vpanel)
   
@@ -58,9 +58,9 @@ def main():
 	i=1
 	thread.start_new_thread(adquisicion,(i,))
 	while True:
-		if S==1: 
+		if sw==1: 
 			print("dato leido")
-			S=0
+			sw=0
 		else: 
 			print("dato no leido")
 	
