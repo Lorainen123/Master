a085a1a2c9beee17895f8b04daf67c3a7a1d02d1
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-23 12:08:43 -0500

    Update Node611.py

--- Node611.py
+++ Node611.py
@@ -277,25 +277,8 @@ def sensorm():
  #   time.sleep(0.01)
 
 
-sw = True
-while sw==True:
-    print('Ingrese 1 para ajustar voltaje, 2 para descargar historicos')
-    x = input()
-    if x==1:
-        sw1 = True
-        while sw1==True:
-              print('Ingrese el valor de voltaje entre el rango de 14.5 y 18.6')
-              v = input()
-              n = excel.main(float(v),0)
-	      n = int(n)
-              mcpras.set_value(n)
-              print('Para salir ingrese 3, para seguir ajustando 1')
-              y = input()
-              if y==3:
-                 sw1=False
-              elif y==1:
-                 sw1= True
 
+        
 
 def main():
 	
