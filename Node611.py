488ab69cf5bd95cdf912cce4d4c50d4c5050a9ec
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-23 12:07:50 -0500

    Update Node611.py

--- Node611.py
+++ Node611.py
@@ -277,13 +277,53 @@ def sensorm():
  #   time.sleep(0.01)
 
 
+sw = True
+while sw==True:
+    print('Ingrese 1 para ajustar voltaje, 2 para descargar historicos')
+    x = input()
+    if x==1:
+        sw1 = True
+        while sw1==True:
+              print('Ingrese el valor de voltaje entre el rango de 14.5 y 18.6')
+              v = input()
+              n = excel.main(float(v),0)
+	      n = int(n)
+              mcpras.set_value(n)
+              print('Para salir ingrese 3, para seguir ajustando 1')
+              y = input()
+              if y==3:
+                 sw1=False
+              elif y==1:
+                 sw1= True
+
 
 def main():
 	
 	while True:
-		GPIO.output(13, True)
-		GPIO.output(19, False)
-		GPIO.output(26, False)
+		print('Ingrese el siguiente estado del sistema:')
+		x = input()
+		if x==1:
+			GPIO.output(13, False)
+			GPIO.output(19, False)
+			GPIO.output(26, False)
+		elif x==2:
+			GPIO.output(13, False)
+			GPIO.output(19, False)
+			GPIO.output(26, True)
+		elif x==3:
+			GPIO.output(13, False)
+			GPIO.output(19, True)
+			GPIO.output(26, False)
+		elif x==4:
+			GPIO.output(13, False)
+			GPIO.output(19, True)
+			GPIO.output(26, True)
+		elif x==5:
+			GPIO.output(13, True)
+			GPIO.output(19, False)
+			GPIO.output(26, False)
+			
+			
 		
 main()
 
