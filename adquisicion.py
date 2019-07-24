22c8a167914b75080ac7c9ba3f9824bc74a966e4
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-24 15:19:31 -0500

    Update adquisicion.py

--- adquisicion.py
+++ adquisicion.py
@@ -162,12 +162,12 @@ def adquisicion():
 			
 		
 		sw=1
-		print("Potencia del panel = "+str(PStotal))
-		print("Corriente Panel = "+str((IpanelT/j)-0.5))
-		print("Voltaje Panel = "+str(VpanelT/j))
-		print("Potencia de la bat = "+str(PBtotal))
-		print("Potencia de la carga = "+str(PLtotal))
-		print("Potencia de la Red = "+str(Pred))
+		#print("Potencia del panel = "+str(PStotal))
+		#print("Corriente Panel = "+str((IpanelT/j)-0.5))
+		#print("Voltaje Panel = "+str(VpanelT/j))
+		#print("Potencia de la bat = "+str(PBtotal))
+		#print("Potencia de la carga = "+str(PLtotal))
+		
 	#	time.sleep(0.001)
 		
 			
@@ -184,6 +184,7 @@ def adquisicion2():
 			#PRtotal=round(PRtotal,2)
 			##potencia de la red
 			Pred=round(6.8807+1.06223*PRtotal+0.00221977*PRtotal*PRtotal,3)
+			print("Potencia de la Red = "+str(Pred))
 		#	if Pred<1:
 		#		Pred=0
 		#	elif Pred<7+7*0.1 and Pred>7-7*0.1:
