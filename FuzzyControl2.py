4eeaea5859a5b6d29aba52d15d016e16ea7f10d4
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-07-30 09:56:59 -0500

    Update FuzzyControl2.py

--- FuzzyControl2.py
+++ FuzzyControl2.py
@@ -59,7 +59,7 @@ Vdif = ctrl.Antecedent(np.arange(-2, 2, 0.1),'Vdif')
 #Dired 
 dired['NB'] = fuzz.trapmf(dired.universe, [-2, -0.525, -0.07, -0.04])
 dired['NS'] = fuzz.trimf(dired.universe, [-0.07, -0.034, 0])
-dired['Z'] = fuzz.trimf(dired.universe, [-0.034, 0, 0.034])
+dired['Z'] = fuzz.trapmf(dired.universe, [-0.034, -0.003, 0, 0.003, 0.034])
 dired['PS'] = fuzz.trimf(dired.universe, [0, 0.034, 0.07])
 dired['PB'] = fuzz.trapmf(dired.universe, [0.04, 0.07, 0.525, 2])
 
