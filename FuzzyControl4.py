7daa9792bc7849da9cb5d899d084461694ec043c
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-02 16:40:30 -0500

    Update FuzzyControl4.py

--- FuzzyControl4.py
+++ FuzzyControl4.py
@@ -249,8 +249,8 @@ def corrienteRed():
      					ired2 = ina2.power()/1000
         				#ired3 = ina3.power()/1000
 	      				Itotal=ired+ired1+ired2+ired2
-	      				#Itotal=round(Itotal,3)
-					#Pred[i]=6.8807+1.06223*Itotal+0.00221977*Itotal*Itotal
+	      				Itotal=round(Itotal,3)
+					Pred[i]=6.8807+1.06223*Itotal+0.00221977*Itotal*Itotal
 					#print(Pred[2])
 					#PtotalT=PtotalT+Pred
 					#if i>=1:
@@ -271,8 +271,8 @@ def corrienteRed():
      					ired2 = ina2.power()/1000
         				#ired3 = ina3.power()/1000
 	      				Itotal=ired+ired1+ired2+ired2
-	      				#Itotal=round(Itotal,3)
-					#Pred[i]=6.8807+1.06223*Itotal+0.00221977*Itotal*Itotal
+	      				Itotal=round(Itotal,3)
+					Pred[i]=6.8807+1.06223*Itotal+0.00221977*Itotal*Itotal
 					#print(Pred[0])
 					#if i>=1:
 						#if Pred[i]<0.9*Pred[i-1] or Pred[i]>1.1*Pred[i-1]:
