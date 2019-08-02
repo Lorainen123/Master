c1da15dbb04a368eb20f5eec7cebf24f344fd789
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-02 17:34:19 -0500

    Update FuzzyControl4.py

--- FuzzyControl4.py
+++ FuzzyControl4.py
@@ -442,16 +442,16 @@ def main():
 
 
 #main()
-#hilo1=threading.Thread(target=fuzzy)
+hilo1=threading.Thread(target=fuzzy)
 #hilo2=threading.Thread(target=main)
 #hilo3=threading.Thread(target=adquisicion)
-#hilo1.start()
+hilo1.start()
 #hilo2.start()
 #hilo3.start()
  
-while True:
-	Pred=potenciaRed()
-	print(Pred)
+#while True:
+#	Pred=potenciaRed()
+#	print(Pred)
 	
    # dpdv=Pdif/Vdif
        
