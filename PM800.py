99390751a8b010e44396761c5ce40a8ff0420a85
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-05 20:12:34 -0500

    Update PM800.py

--- PM800.py
+++ PM800.py
@@ -1,14 +1,14 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-client = ModbusClient(method='rtu', port= '/dev/ttyUSB0', baudrate= 9600)
 
+client = ModbusClient(method='rtu', port= '/dev/ttyUSB0', bytesize=8, timeout=1, baudrate= 9600)
 if client.connect():
     
     print("puerto abierto")
 else:
     print("puerto no abierto")
 
-pmC1 = client.read_holding_registers(1100, 1, unit=2)#Current A 1100
+pmC1 = client.read_holding_registers(1099, 1, unit=1)#Current A 1100
 
 C1 = pmC1.registers[0]/1000
 
