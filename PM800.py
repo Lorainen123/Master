c0f4635f6374f6150d87c94d9194b6a5a0e94192
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-06 14:23:54 -0500

    Update PM800.py

--- PM800.py
+++ PM800.py
@@ -8,7 +8,7 @@ if client.connect():
 else:
     print("puerto no abierto")
 
-pmC1 = client.read_holding_registers(11712, 10, unit=1)#Current A 1100
+pmC1 = client.read_holding_registers(11720, 10, unit=1)#Current A 1100
 
 C1 = pmC1.registers[0]
 C2 = pmC1.registers[1]
