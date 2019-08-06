64682bd1b6e342046d44ef9412f49b7bbf872b75
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-05 19:35:22 -0500

    Update PM800.py

--- PM800.py
+++ PM800.py
@@ -1,6 +1,6 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-client = ModbusClient(method='rtu', port= '/dev/ttyUSB0', baudrate= 9600)
+client = ModbusClient(method='rtu', port= '/dev/ttyUSB0', baudrate= 19200)
 
 if client.connect():
     
