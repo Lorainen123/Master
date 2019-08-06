2db0d3fcaf150a8fd22723446ec853ed7fc45224
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-05 19:13:06 -0500

    Update PM800.py

--- PM800.py
+++ PM800.py
@@ -1,5 +1,7 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
 client = ModbusClient(method='rtu', port= '/dev/ttyUSB09', bytesize=8, timeout=1, baudrate= 9600)
-client.connect()
-print(client)
+
+if client.connect():
+    
+    print("puerto abierto")
