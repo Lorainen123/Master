48757d87431ec521ccd4bcdec4961b9d79b1124e
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-06 18:49:18 -0500

    Update PM800.py

--- PM800.py
+++ PM800.py
@@ -1,4 +1,8 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.payload import BinaryPayloadBuilder
+
 
 
 client = ModbusClient(method='rtu', port= '/dev/ttyUSB0', bytesize=8, timeout=1, baudrate= 9600)
@@ -8,26 +12,27 @@ if client.connect():
 else:
     print("puerto no abierto")
 
-pmC1 = client.read_holding_registers(11729, 10, unit=1)#Current A 1100
+pmC1 = client.read_holding_registers(11729, 1, unit=1)#Current A 1100
+decoder = BinaryPayloadDecoder.fromRegisters(pmC1.registers, endian=Endian.Little)
+print(decoder.decode_32bit_float())
+#C1 = pmC1.registers[0]
+#C2 = pmC1.registers[1]
+#C3 = pmC1.registers[2]
+#C4 = pmC1.registers[3]
+#C5 = pmC1.registers[4]
+#C6 = pmC1.registers[5]
+#C7 = pmC1.registers[6]
+#C8 = pmC1.registers[7]
+#C9 = pmC1.registers[8]
+#C10 = pmC1.registers[9]
 
-C1 = pmC1.registers[0]
-C2 = pmC1.registers[1]
-C3 = pmC1.registers[2]
-C4 = pmC1.registers[3]
-C5 = pmC1.registers[4]
-C6 = pmC1.registers[5]
-C7 = pmC1.registers[6]
-C8 = pmC1.registers[7]
-C9 = pmC1.registers[8]
-C10 = pmC1.registers[9]
-
-print(C1)
-print(C2)
-print(C3)
-print(C4)
-print(C5)
-print(C6)
-print(C7)
-print(C8)
-print(C9)
-print(C10)
+#print(C1)
+#print(C2)
+#print(C3)
+#print(C4)
+#print(C5)
+#print(C6)
+#print(C7)
+#print(C8)
+#print(C9)
+#print(C10)
