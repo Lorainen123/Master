6565fc1ffda294fa9521ce1ca84010b488733980
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-06 19:50:43 -0500

    Update PM800.py

--- PM800.py
+++ PM800.py
@@ -14,7 +14,7 @@ else:
     print("puerto no abierto")
 
 result = client.read_holding_registers(11729, 2, unit=1)#Current A 1100
-decoder = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big, wordorder=Endian.Big )
+decoder = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big, wordorder=Endian.Litte )
 print(decoder.decode_32bit_float())
 
 
