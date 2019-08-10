883c20766234407599d6a5f1ca48c688600c2cfd
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-10 15:59:51 -0500

    Update FuzzyControl4.py

--- FuzzyControl4.py
+++ FuzzyControl4.py
@@ -495,9 +495,9 @@ def state1T():
 	global state
 	if  Pred<PLtotal and Pred>8:
 		state='1T'
-	elif Pred<8:
+	elif Pred<8 and PStotal>0:
 		state=2
-	elif PStotal<=0 and Pred<=8.1:
+	elif PStotal<=0 and Pred<=0:
 		state=3
 	
 def state2():
