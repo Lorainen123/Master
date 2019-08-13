6484175f948d40e3cc3ab6abb8bc553f90af5764
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-13 10:31:52 -0500

    Update adquisicion.py

--- adquisicion.py
+++ adquisicion.py
@@ -153,7 +153,7 @@ def adquisicion():
 		
 	#	toc = tm.default_timer()
 		## potencia del panel solar			
-		PStotal=round(((IpanelT-0.5)*VpanelT)/(j*j),2) ## potencia del panel solar promedio
+		PStotal=round((IpanelT)*((VpanelT+0.5)/j)/j,2) ## potencia del panel solar promedio
 		
 		
 		## potencia de la carga 
@@ -177,7 +177,7 @@ def adquisicion():
 		PTred=decoder.decode_32bit_float()
 		sw=1
 		print("Potencia del panel = "+str(PStotal))
-		print("Corriente Panel = "+str((IpanelT/j)-0.5))
+		print("Corriente Panel = "+str((IpanelT/j)))
 		print("Voltaje Panel = "+str((VpanelT/j)+0.5))
 		print("Potencia de la bat = "+str(PBtotal))
 		print("Potencia de la carga = "+str(PLtotal))
