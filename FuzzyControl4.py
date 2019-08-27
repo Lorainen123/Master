3933b7d981db2790fdbe9581c14b5da532d5d663
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-08-27 11:46:34 -0500

    Update FuzzyControl4.py

--- FuzzyControl4.py
+++ FuzzyControl4.py
@@ -156,18 +156,18 @@ if client.connect():
 else:
     print("puerto no abierto")
 
-#libro = xlsxwriter.Workbook('Experimento6.xlsx')
-#hoja = libro.add_worksheet()
-#hoja.write(0, 0,     "Estado")	
-#hoja.write(0, 1,     "Voltaje Panel")
-#hoja.write(0, 2,     "Corriente Panel")
-#hoja.write(0, 3,     "Potencia de la red")
-#hoja.write(0, 4,     "Potencia de la carga")
-#hoja.write(0, 5,     "Potencia de la bateria")
-#hoja.write(0, 6,     "Hora")
+libro = xlsxwriter.Workbook('Experimento6.xlsx')
+hoja = libro.add_worksheet()
+hoja.write(0, 0,     "Estado")	
+hoja.write(0, 1,     "Voltaje Panel")
+hoja.write(0, 2,     "Corriente Panel")
+hoja.write(0, 3,     "Potencia de la red")
+hoja.write(0, 4,     "Potencia de la carga")
+hoja.write(0, 5,     "Potencia de la bateria")
+hoja.write(0, 6,     "Hora")
 
-def adquisicion():
- 
+#def adquisicion():
+try:
 	global PStotal, PLtotal, PTred, PBtotal, sw, state, VpanelT, IpanelT
 	k=1
 	while True:
@@ -281,19 +281,19 @@ def adquisicion():
 		print("Potencia de la carga = "+str(PLtotal))
 		print(state)
 		
-	#	hoja.write(k, 0,     str(state))
+		hoja.write(k, 0,     str(state))
 		
-	#	hoja.write(k, 1,     str(VpanelT))
+		hoja.write(k, 1,     str(VpanelT))
 		
-	#	hoja.write(k, 2,     str(IpanelT))
+		hoja.write(k, 2,     str(IpanelT))
 		
-	#	hoja.write(k, 3,     str(PTred))
+		hoja.write(k, 3,     str(PTred))
 		
-	#	hoja.write(k, 4,     str(PLtotal))
+		hoja.write(k, 4,     str(PLtotal))
 		
-	#	hoja.write(k, 5,     str(PBtotal))
+		hoja.write(k, 5,     str(PBtotal))
 				
-	#	hoja.write(k, 6,     time.strftime("%X"))
+		hoja.write(k, 6,     time.strftime("%X"))
 		Estados(state)
 		
 		
@@ -315,7 +315,8 @@ def adquisicion():
 #				GPIO.output(13, False)
 #				GPIO.output(19, True)
 #				GPIO.output(26, False)
- 
+execpt:
+	libro.close()
 				
 def adquisicion2():
 	global PTred
@@ -686,12 +687,12 @@ def main():
 #main()
 #hilo1=threading.Thread(target=fuzzy)
 #hilo2=threading.Thread(target=main)
-hilo3=threading.Thread(target=adquisicion)
+#hilo3=threading.Thread(target=adquisicion)
 #hilo4=threading.Thread(target=adquisicion2)
 
 #hilo1.start()
 #hilo2.start()
-hilo3.start()
+#hilo3.start()
 #hilo4.start()
  
 #while True:
