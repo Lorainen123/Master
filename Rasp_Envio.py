0e06c2a75a6f878f1020148930dfc208b6a941e0
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-09-09 09:21:43 -0500

    Update Rasp_Envio.py

--- Rasp_Envio.py
+++ Rasp_Envio.py
@@ -7,12 +7,26 @@ from datetime import datetime
 UDP_IP = "181.192.172.207"
 UDP_PORT = 5011
 
+#UDP_IP2 = "190.143.58.202"
+#UDP_PORT2 = 5011
+
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
+
+#print("UDP target IP 2:", UDP_IP2)
+#print("UDP target port 2:", UDP_PORT2)
 
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+
+#try: 
+#    sock = socket.socket(socket.AF_INET, # Internet
+#                     socket.SOCK_DGRAM) # UDP
+#except socket.error:
+#    print("Could not bind 2nd IP")
+  
+  
 while True:
 #    MESSAGE = str(adquisicion())    
     now = datetime.now()
