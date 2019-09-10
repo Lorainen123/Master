2313a7a55bfeef12c1908ff5d9833017679daa63
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-09-10 10:10:26 -0500

    Update FuzzyControl4.py

--- FuzzyControl4.py
+++ FuzzyControl4.py
@@ -158,7 +158,7 @@ else:
 
 libro = xlsxwriter.Workbook('Fuzzy1.xlsx')
 hoja = libro.add_worksheet()
-libro2 = xlsxwriter.Workbook('Fuzzy2.xlsx')
+libro1 = xlsxwriter.Workbook('Fuzzy2.xlsx')
 hoja1 = libro2.add_worksheet()
 hoja.write(0, 0,     "Estado")	
 hoja.write(0, 1,     "Voltaje Panel")
