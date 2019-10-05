4677e2035d4fddc51320ea762b66bf15b1aea601
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-10-05 15:08:56 -0500

    Update Pruebas.py

--- Pruebas.py
+++ Pruebas.py
@@ -32,7 +32,7 @@ v=14.5
 #archivo=open("prueba1.txt","w")
 Predv = np.zeros(5,)
 Pred=0
-libro = xlsxwriter.Workbook('caracterizacion3.xlsx')
+libro = xlsxwriter.Workbook('caracterizacion4.xlsx')
 hoja = libro.add_worksheet()
 
 hoja.write(0, 0,     "No prueba")	
