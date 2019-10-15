bcdd40ba0a32a4f405c39f575da57d3788cf3d76
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-10-15 17:30:38 -0500

    Update FuzzyControl4.py

--- FuzzyControl4.py
+++ FuzzyControl4.py
@@ -157,7 +157,7 @@ if client.connect():
 else:
     print("puerto no abierto")
 
-libro = xlsxwriter.Workbook('prueba_estados3.xlsx')
+libro = xlsxwriter.Workbook('prueba_estados4.xlsx')
 hoja = libro.add_worksheet()
 
 hoja.write(0, 0,     "Estado")	
