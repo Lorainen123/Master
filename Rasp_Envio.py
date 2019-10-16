916ca2ffb21f5fbd6b6f14974fd7825318d3e575
Julian9875 <43647876+Julian9875@users.noreply.github.com>
2019-10-16 07:58:14 -0500

    Añadido estado 5

--- Rasp_Envio.py
+++ Rasp_Envio.py
@@ -1,28 +1,20 @@
 import socket
 from n611_adquisicion import *
+from FuzzyControl4 import *
 import time
 from datetime import datetime
 from datetime import timedelta
 
-
+state=1
 UDP_IP = "3.89.222.210"
 UDP_PORT = 5000
-#UDP_IP2 = "190.143.58.202"
-#UDP_PORT2 = 5011
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
-#print("UDP target IP 2:", UDP_IP2)
-#print("UDP target port 2:", UDP_PORT2)
 
+def SendData():
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-#try:
-#    sock = socket.socket(socket.AF_INET, # Internet
-#                     socket.SOCK_DGRAM) # UDP
-#except socket.error:
-#    print("Could not bind 2nd IP")
-while True:
 #    MESSAGE = str(adquisicion())
     try:
         now = datetime.now()
@@ -31,7 +23,7 @@ while True:
         ADQ=adquisicion()
         ADQ=str(ADQ)
         ADQ=ADQ[1:len(ADQ)-1]
-        MESSAGE = ADQ+', ' + fecha +', '+ hora
+        MESSAGE = ADQ+', ' + fecha +', '+ hora + state
         MESSAGE = bytes(MESSAGE)
         print("message:", MESSAGE)
         sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
@@ -40,3 +32,19 @@ while True:
         print "Error de Conexión, reintentando..."
         time.sleep(1)
     time.sleep(5)
+    
+    return sock
+        
+def ReceiveData(sock):
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print ("received message:", data)
+    if (data="True"):
+        to5=True
+    else:
+        to5=False
+    
+while True:
+    Estados(state,to5)
+    sock=SendData()
+    ReceiveData(sock)
+    
